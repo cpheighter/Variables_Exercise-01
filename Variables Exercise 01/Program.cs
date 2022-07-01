89646072562d0896a564e3009191a8cc81f5db65
cpheighter <cheighterjr@gmail.com>
2022-07-01 13:48:13 -0400

    Declared and initialize constants

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -10,7 +10,9 @@ namespace Variables_Exercise_01
             // - Once finished save, commit, and then push to GitHub!
 
             // TODO: Declare 3 constansts
-
+            const string College = "Havard";
+            const int Year = 2022;
+            const decimal GPA = 4.0m;
             #region Types
             // string
             // char
@@ -21,11 +23,14 @@ namespace Variables_Exercise_01
             #endregion
 
             // TODO: Declare a variable for each of the types above
+
+
             // Make sure to use C# naming convention best practices 
 
             // TODO: Initialize each of the variables with a value
 
             // TODO: Write out each of the variables to the Console 
+            System.Console.WriteLine("Student B attends " + College + " in " + Year + " with a " + GPA + " GPA");
         }
 
     }
